b23d6f4e01f40cf4d1ea1df2beac2a233c6b97af
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-11-03 01:31:21 +0000

    Continue screwing around

--- pageres/pageres.js
+++ pageres/pageres.js
@@ -1,21 +1,26 @@
-const Pageres = require('pageres');
+#!/usr/bin/env node
 const path = require('path');
+const Pageres = require('pageres');
+const rimraf = require('rimraf');
 
 const screenshotsPath = path.join(__dirname, 'screenshots');
-const resolutions = [
-  '640x360',
-  '1920x1080',
-  '1366x76',
-  '1024x76',
-];
+const resolutions = ['640x360', '1920x1080', '1366x76', '1024x76'];
+const url = process.argv[2] || 'http://sajadtorkamani.com';
 
 (async () => {
   console.log('Working...');
 
+  // Clear existing screenshots
+  rimraf.sync(screenshotsPath);
+
+  // Go and capture new ones!
   await new Pageres({ delay: 2 })
-    .src('https://sajadtorkamani.com', resolutions)
+    .src(
+      url,
+      resolutions
+    )
     .dest(screenshotsPath)
     .run();
 
-  console.log('Done.');
+  console.log(`Done! Screenshots saved to ${screenshotsPath}`);
 })();

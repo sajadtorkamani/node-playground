6aab919af0a031e561601ac499910ac828ed656e
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-11-03 02:22:08 +0000

    Prettify output

--- is-up/is-up.js
+++ is-up/is-up.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const isUp = require('is-up');
 const Table = require('cli-table3');
+const colors = require('colors/safe');
 
 const sites = [
   'https://sajadtorkamani.com',
@@ -9,7 +10,7 @@ const sites = [
 ];
 
 const table = new Table({
-  head: ['Url', 'Status']
+  head: [colors.white('Url'), colors.white('Status')]
 });
 
 (async () => {
@@ -18,7 +19,7 @@ const table = new Table({
   for (let site of sites) {
     const isSiteUp = await isUp(site);
 
-    table.push([site, isSiteUp ? 'Up' : 'Down']);
+    table.push([site, isSiteUp ? colors.green('Up') : colors.red('Down')]);
   }
 
   console.log(table.toString());
